236ee0ef12d7f02a2547061de74b5f327df2a459
shaikgul <gulnazshaik10@gmail.com>
2022-06-06 22:20:37 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-//import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
 
-function App() {
-  return (
-    <div className="App">
-      <b className='userName'>gulnaz</b>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+    <Router>
+        <div >
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav >
+          <ul >
+            <li><Link to={'/'} > Homes </Link></li>
+            <li><Link to={'/contact'} >Contact</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+              <Route exact path='/' element={<Home/>} />
+              <Route path='/contact' element={<Contact/>} />
+              <Route path='/about' element={<About/>} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+
+export default App
